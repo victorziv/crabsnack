c867548efebc45627641b83254a755b60d25c150
Victor Ziv <vziv@infinidat.com>
2017-03-14 17:26:25 +0200

    Change entry_points format in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=['cli'],
     include_package_data=True,
     install_requires=['click'],
-    entry_points="""
-        [console_scripts]
-        factory=cli:cli
-    """,
+    entry_points={
+        'console_scripts': ['runcli=cli:cli']
+    },
 )
