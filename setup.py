d8f4b693db57ad1b4d88a333aef5b02b87de2102
Victor Ziv <vziv@infinidat.com>
2017-03-18 12:11:49 +0200

    Start formatting setup.py for pytest usage

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 import functools
 
-_in_dir = functools.partial(os.path.join, os.path.dirname(__file__)) # bla bla bla bla 
+_in_dir = functools.partial(os.path.join, os.path.dirname(__file__))
 
 with open(_in_dir("crabsnack", "version.py")) as vf:
     exec(vf.read())
@@ -15,17 +17,28 @@ def readme():
 # ___________________________________
 
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '-tb=long', 'crabsnack/tests']
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+# ___________________________________
+
+
 setup(
     name='Crabsnack',
-    version=__version__, # noqa
+    version=__version__,  # noqa
     description='The best sea food in the world',
     long_description=readme(),
 
     classifiers=[
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
 
     include_package_data=True,
@@ -39,5 +52,7 @@ setup(
         'markdown',
     ],
     test_suite='pytest',
-    test_require=['pytest']
+    test_require=['pytest'],
+
+
 )
