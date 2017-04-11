6337437ba629b026f76a7f1704700a7b61aa30cc
Victor Ziv <victor@colabo.com>
2017-04-11 20:21:29 +0000

    Start handle login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
-from ..models import User
+from ..models import UserModel
 
 # ====================================
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
@@ -12,32 +14,38 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 # ====================================
 
+
 class RegistrationForm(FlaskForm):
 
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     username = StringField(
-        'Username', 
-        validators=[ 
-            Required(), 
-            Length(1, 64), 
-            Regexp( '^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, dots or underscores')
+        'Username',
+        validators=[
+            Required(),
+            Length(1, 64),
+            Regexp(
+                '^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                'Usernames must have only letters, numbers, dots or underscores'
+            )
         ]
     )
 
-    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+    password = PasswordField(
+        'Password',
+        validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+
     password2 = PasswordField('Confirm password', validators=[Required()])
     submit = SubmitField('Register')
-
     # _____________________________
 
     def validate_email(self, field):
-        user = User()
-        if user.exists_by_email(email=field.data):
+        um = UserModel()
+        if um.exists_by_email(email=field.data):
             raise ValidationError('Email already registered.')
 
     # _____________________________
 
     def validate_username(self, field):
-        user = User() 
-        if user.exists_by_username(username=field.data):
+        um = UserModel()
+        if um.exists_by_username(username=field.data):
             raise ValidationError('Username already in use.')
