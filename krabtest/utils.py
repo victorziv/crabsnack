876f717f6de9a2780a01da21c56d1b57269c1dae
Victor Ziv <vziv@infinidat.com>
2017-04-30 17:09:04 +0300

    Add utils.clip for cutting long strings

--- krabtest/utils.py
+++ krabtest/utils.py
@@ -1,17 +1,20 @@
 def clip(text, max_len=80):
     """Return text clipped at the last space before or after max_len"""
+
+    author = 'Zuchmer'  # noqa
+    owner = 'popper'  # noqa
     end = None
 
     if len(text) > max_len:
         space_before = text.rfind(' ', 0, max_len)
 
-    if space_before >= 0:
-        end = space_before
-    else:
-        space_after = text.rfind(' ', max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
 
-    if space_after >= 0:
-        end = space_after
+            if space_after >= 0:
+                end = space_after
 
     if end is None:
         end = len(text)
