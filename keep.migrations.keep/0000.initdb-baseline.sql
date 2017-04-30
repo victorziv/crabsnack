8381c20451a270f767635f738589a4094affbf73
Victor Ziv <vziv@infinidat.com>
2017-04-30 17:16:27 +0300

    Remove redundant migrations folders

--- keep.migrations.keep/0000.initdb-baseline.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS changelog (
-    id serial PRIMARY KEY,
-    name VARCHAR(64) UNIQUE,
-    filenumber VARCHAR(4),
-    dateapplied TIMESTAMP,
-    comment VARCHAR(255)
-);
-
-
--- keep.migrations.keep/0001.create_table-installationstep.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS installationstep (
-    id serial PRIMARY KEY,
-    name VARCHAR(32) UNIQUE,
-    display_name VARCHAR(64),
-    priority INTEGER
-);
