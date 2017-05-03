1908a9e584c7909b0cc9d2723deffec5d5e29b7d
Victor Ziv <vziv@infinidat.com>
2017-05-03 18:40:35 +0300

    Use WTF login form with addition of Google OAuth button

--- app/auth/views.py
+++ app/auth/views.py
@@ -81,3 +81,22 @@ def logout():
 #    flash("You have been logged out!")
     return redirect(url_for('main.index'))
 # __________________________________________
+
+
+# @auth.route('/reset', methods=['GET', 'POST'])
+# def password_reset_request():
+#     if not current_user.is_anonymous:
+#         return redirect(url_for('main.index'))
+#     form = PasswordResetRequestForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         if user:
+#             token = user.generate_reset_token()
+#             send_email(user.email, 'Reset Your Password',
+#                        'auth/email/reset_password',
+#                        user=user, token=token,
+#                        next=request.args.get('next'))
+#         flash('An email with instructions to reset your password has been '
+#               'sent to you.')
+#         return redirect(url_for('auth.login'))
+#     return render_template('auth/reset_password.html', form=form)
