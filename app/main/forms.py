fb787cac53da2db31944d417cdddcb5078f61f8c
Victor Ziv <vziv@infinidat.com>
2017-05-09 21:08:45 +0300

    Add edit profile button

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Required
+from wtforms import StringField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Required, Length
 
 
 class LoginForm(FlaskForm):
@@ -13,3 +13,12 @@ class LoginForm(FlaskForm):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+# ===============================
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('User name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+# ===============================
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort
+from flask import render_template, session, redirect, url_for, abort, flash
 from . import main
-from .forms import NameForm
-from flask_login import login_required
+from .forms import NameForm, EditProfileForm
+from flask_login import login_required, current_user
 from ..models import Permission, User
 from ..decorators import admin_required, permission_required
 # _______________________________
@@ -49,3 +49,22 @@ def user_profile(email):
     if u is None:
         abort(404)
     return render_template('user_profile.html', user=u, current_time=datetime.utcnow())
+# _______________________________
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        User().save_user(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user_profile', username=current_user.username))
+
+    form.username.data = current_user.username
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
