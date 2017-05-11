102382799fe36ea1ad4bb1e13e46459e6411bd14
Victor Ziv <vziv@infinidat.com>
2017-05-11 19:49:47 +0300

    Admin user can edit all fields of her own profile, but what about other users

--- app/main/views.py
+++ app/main/views.py
@@ -32,14 +32,14 @@ def index():
 @main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
 @login_required
 @admin_required
-def edit_profile_admin():
+def edit_profile_admin(id):
     user = User.get_by_field_or_404(name='id', value=id)
     form = EditProfileAdminForm(user=user)
 
     if form.validate_on_submit():
         user.email = form.email.data
         user.username = form.username.data
-        user.confirmed = form.confirmed.data
+#         user.confirmed = form.confirmed.data
         user.role = Role.query.get(form.role.data)
         user.location = form.location.data
         user.about_me = form.about_me.data
@@ -49,13 +49,11 @@ def edit_profile_admin():
 
     form.email.data = user.email
     form.username.data = user.username
-    form.confirmed.data = user.confirmed
-    form.role.data = user.role_id
-    form.name.data = user.name
+#     form.confirmed.data = user.confirmed
+    form.role.data = user.role
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
-
 # _______________________________
 
 
