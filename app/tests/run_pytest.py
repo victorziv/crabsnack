93f0ebcad27ddf3ef9823e8138dee2f556460921
Victor Ziv <victor@colabo.com>
2017-05-28 23:21:04 +0300

    Add parse_args() to run_pytest

--- app/tests/run_pytest.py
+++ app/tests/run_pytest.py
@@ -1,4 +1,6 @@
+import argparse
 from app.dbmodels.query_admin import DBAdmin
+# __________________________________
 
 
 def resetdb(conf):
@@ -10,3 +12,20 @@ def resetdb(conf):
         dba.createdb()
     finally:
         conn.close()
+# __________________________________
+
+
+def parse_args():
+    d = "Trigger one or more unittests"
+    parser = argparse.ArgumentParser(description=d)
+    parser.add_argument(
+        '-t', '--type', dest='apptype', default='testing',
+        help="""Application type: testing, development, production. Default: testing""")
+
+    return parser.parse_args()
+# __________________________________
+
+
+def main():
+    opts = parse_args()
+    print(opts)
