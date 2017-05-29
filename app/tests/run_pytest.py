3f6486a3fd6f152d784ef764298d1b5ca3f2380c
Victor Ziv <vziv@infinidat.com>
2017-05-29 22:41:50 +0300

    Run a single test module ( hard coded)

--- app/tests/run_pytest.py
+++ app/tests/run_pytest.py
@@ -48,7 +48,7 @@ def main():
     print(f"Configuration key: {opts.configkey}")
     conf = config[opts.configkey]
     migratedb(conf)
-    pytest.main(['-x', os.path.join(conf.BASEDIR, 'app', 'tests')])
+    pytest.main(['-x', os.path.join(conf.BASEDIR, 'app', 'tests', 'test_follow.py')])
 # __________________________________
 
 
