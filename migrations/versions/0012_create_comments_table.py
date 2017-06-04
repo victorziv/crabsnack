a2db0e60e5c2ba2e127a89d675d59354a26c1ab2
Victor Ziv <victor@colabo.com>
2017-06-05 00:23:34 +0300

    comments table

--- migrations/versions/0012_create_comments_table.py
+++ migrations/versions/0012_create_comments_table.py
@@ -7,7 +7,7 @@ def create_table_comments(conn):
             id serial PRIMARY KEY,
             body TEXT,
             body_html TEXT,
-            commentdate TIMESTAMP WITHOUT TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
+            commentdate TIMESTAMP WITHOUT TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
             author_id INTEGER NOT NULL,
             post_id INTEGER NOT NULL,
             UNIQUE(author_id, post_id)
